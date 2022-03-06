2eb25ffcd1864e41a7f63dc7f0d586446cd7bce7
Nesreen-ahmed <nesrenahmed2710@gmail.com>
2022-03-07 01:36:24 +0200

    updated

---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    float speed = 50;
+    float rotation_speed = 100;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+         if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+         if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+         if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0,-rotation_speed*Time.deltaTime,0));
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0,rotation_speed*Time.deltaTime,0));
+        }
+    }
+}
