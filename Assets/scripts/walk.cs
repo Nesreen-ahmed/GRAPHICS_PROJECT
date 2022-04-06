b4d58a65b24d370bd4b57d3c660649505b003022
EtafQudsi <etafkodsi@gmail.com>
2022-04-06 23:19:38 +0200

    end

--- Assets/scripts/walk.cs
+++ Assets/scripts/walk.cs
@@ -8,19 +8,23 @@ public class walk : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey("up"))
         {
             theNPC.GetComponent<Animator>().Play("Walking");
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        //else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKey("down"))
         {
             theNPC.GetComponent<Animator>().Play("Walking Backwards");
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        //else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKey("left"))
         {
             theNPC.GetComponent<Animator>().Play("Left Strafe Walking");
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        //else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey("right"))
         {
             theNPC.GetComponent<Animator>().Play("Right Strafe Walking");
         }
