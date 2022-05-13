97faadafebf83627ce6f3711e51966d35478c615
rahmaeid182 <rhma.eid.eldeeb@gmail.com>
2022-05-13 19:31:34 +0200

    Done

---
+++ Assets/scripts/ChangeCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public float sec;
+    public GameObject fromCamera;
+    public GameObject toCamera;
+    AudioListener cameraOneAudioLis;
+    AudioListener cameraTwoAudioLis;
+    void Start()
+    {
+        StartCoroutine(ExecuteAfterTime(sec));
+
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        toCamera.SetActive(true);
+        fromCamera.SetActive(false);
+    }
+}
