b79fa90aa6af009c4adea22a263b8d471b7f7dbe
EtafQudsi <etafkodsi@gmail.com>
2022-05-16 01:06:32 +0200

    Etaf

--- Assets/scripts/scenechange.cs
+++ Assets/scripts/scenechange.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class scenechange : MonoBehaviour
 {
-   public void changescene(string scenename)
+
+    public string scenename;
+   void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(scenename);
     }
