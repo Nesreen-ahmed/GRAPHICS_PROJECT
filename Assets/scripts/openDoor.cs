523b7f54212a25202e0ab5f81cc7bcebbe3a186e
Nesreen Ahmed El Tohamy <83316246+Nesreen-ahmed@users.noreply.github.com>
2022-05-16 23:41:29 +0200

    final

--- Assets/scripts/openDoor.cs
+++ Assets/scripts/openDoor.cs
@@ -5,23 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class openDoor : MonoBehaviour
 {
-    public GameObject[] open;
-    public GameObject[] close;
+     public GameObject[] open;
+     public GameObject[] close;
     public GameObject maged;
-     void Awake()
+    void Awake()
     {
         GameObject op = new GameObject();
         op.AddComponent<HappyScore>();
         HappyScore SN = op.GetComponent<HappyScore>();
         string name = SN.GETsceneName();
-        Debug.Log(name);
-        if (name == "happyQuiz")
-        {
+            if (name == "happyQuiz")
+            {
                 open[0].transform.eulerAngles = new Vector3(0f, -90f, 0f);
                 close[0].transform.eulerAngles = new Vector3(0f, -188.338f, 0f);
                  maged.transform.position = new Vector3(234.5f, 1.929326f, 523.4f);
-            maged.transform.eulerAngles = new Vector3(0f, 124.277f, 0f);
-        }
+                 maged.transform.eulerAngles = new Vector3(0f, 124.277f, 0f);
+            }
             else if (name == "sadQuiz")
             {
                 open[1].transform.eulerAngles = new Vector3(0f, 11.078f, 0f);
@@ -31,24 +30,31 @@ public class openDoor : MonoBehaviour
         }
         else if (name == "fearQuiz")
             {
-                open[2].transform.eulerAngles = new Vector3(0f, -90f, 0f);
-                close[2].transform.eulerAngles = new Vector3(0f, 82.641f,0f);
-            maged.transform.position = new Vector3(232.6f, 1.5f, 370.4f);
-            maged.transform.eulerAngles = new Vector3(0f, 139.76f, 0f);
+                open[2].transform.eulerAngles = new Vector3(0f, -80f, 0f);
+                close[2].transform.eulerAngles = new Vector3(0f, 170f, 0f);
+                maged.transform.position = new Vector3(232.6f, 1.5f, 370.4f);
+                maged.transform.eulerAngles = new Vector3(0f, 139.76f, 0f);
         }
         else if (name == "discostQuiz")
             {
                 open[3].transform.eulerAngles = new Vector3(0f, 11.078f, 0f);
-                close[3].transform.eulerAngles = new Vector3(0f, 81.508f, 0f);
+                close[3].transform.eulerAngles = new Vector3(0f, 0f, 0f);
                 maged.transform.position = new Vector3(327.8f, 1.5f, 207.3f);
                 maged.transform.eulerAngles = new Vector3(0f, 232.517f, 0f);
         }
         else if (name == "surprizeQuiz")
             {
                 open[4].transform.eulerAngles = new Vector3(0f, -40f, 0f);
-                close[4].transform.eulerAngles = new Vector3(0f, 84.311f, 0f);
+                close[4].transform.eulerAngles = new Vector3(0f, 160f, 0f);
                 maged.transform.position = new Vector3(300f, 1.5f, 122.2f);
                 maged.transform.eulerAngles = new Vector3(0f, 124.277f, 0f);
+        }
+        else if (name == "angryQuiz")
+        {
+            open[4].transform.eulerAngles = new Vector3(0f, -56.591f, 0f);
+            close[0].transform.eulerAngles = new Vector3(0f, 70f, 0f);
+            maged.transform.position = new Vector3(265.6f, 1.5f, 562.2f);
+            maged.transform.eulerAngles = new Vector3(0f, 196.796f, 0f);
         }
     }
 }
